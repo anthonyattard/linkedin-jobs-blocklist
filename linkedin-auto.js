cade1c38f4c7684ad610ea8ba4dddc0adebc1027
Anthony Attard <anthony@anthonyattard.com>
2020-07-07 13:43:44 -0700

    Updated class names

--- linkedin-auto.js
+++ linkedin-auto.js
@@ -19,123 +19,121 @@
   ];
 
   blacklist = blacklist.map(function(item) {
-      return item.toLowerCase();
+    return item.toLowerCase();
   });
 
+function filter() {
+ // console.log(blacklist);
+  $('.job-card-container__company-name').each(function( index ) {
+      let theVal = $( this ).text().trim().toLowerCase();
+      // console.log(theVal);
+      if(blacklist.includes( theVal ) ) {
+          console.log('Hide item from company: ' + theVal);
+          $( this ).closest(".artdeco-list__item").hide();
+      }
+  });
 
-  function filter() {
-   // console.log(blacklist);
-    $('.job-card-search__company-name-link').each(function( index ) {
-        let theVal = $( this ).text().trim().toLowerCase();
-        //console.log(theVal);
-        if(blacklist.includes( theVal ) ) {
-            console.log('Hid item from company: ' + theVal);
-            $( this ).closest(".artdeco-list__item").hide();
+  console.log('Total items: ' + $('.job-card-container__company-name').length);
+  console.log('Remaining items: ' + $('.job-card-container__company-name:visible').length);
+  console.log('Hidden items: ' + $('.job-card-container__company-name:hidden').length);
+}
+
+/*--- waitForKeyElements():  A utility function, for Greasemonkey scripts,
+    that detects and handles AJAXed content.
+
+    Usage example:
+
+        waitForKeyElements (
+            "div.comments"
+            , commentCallbackFunction
+        );
+
+        //--- Page-specific function to do what we want when the node is found.
+        function commentCallbackFunction (jNode) {
+            jNode.text ("This comment changed by waitForKeyElements().");
         }
-    });
 
-    console.log('Total items: ' + $('.job-card-search__company-name-link').length);
-    console.log('Remaining items: ' + $('.job-card-search__company-name-link:visible').length);
-    console.log('Hidden items: ' + $('.job-card-search__company-name-link:hidden').length);
-  }
+    IMPORTANT: This function requires your script to have loaded jQuery.
+*/
+  // waitForKeyElements (".ember-view", filter);
+  // waitForKeyElements (".artdeco-list__item", filter);
+  // waitForKeyElements (".jobs-search-results__list", filter);
+  waitForKeyElements (".job-card-container--clickable", filter);
 
 
-  /*--- waitForKeyElements():  A utility function, for Greasemonkey scripts,
-      that detects and handles AJAXed content.
+function waitForKeyElements (
+  selectorTxt,    /* Required: The jQuery selector string that
+                      specifies the desired element(s).
+                  */
+  actionFunction, /* Required: The code to run when elements are
+                      found. It is passed a jNode to the matched
+                      element.
+                  */
+  bWaitOnce,      /* Optional: If false, will continue to scan for
+                      new elements even after the first match is
+                      found.
+                  */
+  iframeSelector  /* Optional: If set, identifies the iframe to
+                      search.
+                  */
+) {
+  var targetNodes, btargetsFound;
 
-      Usage example:
+  if (typeof iframeSelector == "undefined")
+      targetNodes     = $(selectorTxt);
+  else
+      targetNodes     = $(iframeSelector).contents ()
+                                        .find (selectorTxt);
 
-          waitForKeyElements (
-              "div.comments"
-              , commentCallbackFunction
-          );
+  if (targetNodes  &&  targetNodes.length > 0) {
+      btargetsFound   = true;
+      /*--- Found target node(s).  Go through each and act if they
+          are new.
+      */
+      targetNodes.each ( function () {
+          var jThis        = $(this);
+          var alreadyFound = jThis.data ('alreadyFound')  ||  false;
 
-          //--- Page-specific function to do what we want when the node is found.
-          function commentCallbackFunction (jNode) {
-              jNode.text ("This comment changed by waitForKeyElements().");
+          if (!alreadyFound) {
+              //--- Call the payload function.
+              var cancelFound     = actionFunction (jThis);
+              if (cancelFound)
+                  btargetsFound   = false;
+              else
+                  jThis.data ('alreadyFound', true);
           }
-
-      IMPORTANT: This function requires your script to have loaded jQuery.
-  */
-//  waitForKeyElements (".ember-view", filter);
-//  waitForKeyElements (".artdeco-list__item", filter);
-  waitForKeyElements (".job-card-search--two-pane", filter);
-
-
-
-  function waitForKeyElements (
-    selectorTxt,    /* Required: The jQuery selector string that
-                        specifies the desired element(s).
-                    */
-    actionFunction, /* Required: The code to run when elements are
-                        found. It is passed a jNode to the matched
-                        element.
-                    */
-    bWaitOnce,      /* Optional: If false, will continue to scan for
-                        new elements even after the first match is
-                        found.
-                    */
-    iframeSelector  /* Optional: If set, identifies the iframe to
-                        search.
-                    */
-  ) {
-    var targetNodes, btargetsFound;
-
-    if (typeof iframeSelector == "undefined")
-        targetNodes     = $(selectorTxt);
-    else
-        targetNodes     = $(iframeSelector).contents ()
-                                          .find (selectorTxt);
-
-    if (targetNodes  &&  targetNodes.length > 0) {
-        btargetsFound   = true;
-        /*--- Found target node(s).  Go through each and act if they
-            are new.
-        */
-        targetNodes.each ( function () {
-            var jThis        = $(this);
-            var alreadyFound = jThis.data ('alreadyFound')  ||  false;
-
-            if (!alreadyFound) {
-                //--- Call the payload function.
-                var cancelFound     = actionFunction (jThis);
-                if (cancelFound)
-                    btargetsFound   = false;
-                else
-                    jThis.data ('alreadyFound', true);
-            }
-        } );
-    }
-    else {
-        btargetsFound   = false;
-    }
-
-    //--- Get the timer-control variable for this selector.
-    var controlObj      = waitForKeyElements.controlObj  ||  {};
-    var controlKey      = selectorTxt.replace (/[^\w]/g, "_");
-    var timeControl     = controlObj [controlKey];
-
-    //--- Now set or clear the timer as appropriate.
-    if (btargetsFound  &&  bWaitOnce  &&  timeControl) {
-        //--- The only condition where we need to clear the timer.
-        clearInterval (timeControl);
-        delete controlObj [controlKey]
-    }
-    else {
-        //--- Set a timer, if needed.
-        if ( ! timeControl) {
-            timeControl = setInterval ( function () {
-                    waitForKeyElements (    selectorTxt,
-                                            actionFunction,
-                                            bWaitOnce,
-                                            iframeSelector
-                                        );
-                },
-                300
-            );
-            controlObj [controlKey] = timeControl;
-        }
-    }
-    waitForKeyElements.controlObj   = controlObj;
+      } );
   }
+  else {
+      btargetsFound   = false;
+  }
+
+  //--- Get the timer-control variable for this selector.
+  var controlObj      = waitForKeyElements.controlObj  ||  {};
+  var controlKey      = selectorTxt.replace (/[^\w]/g, "_");
+  var timeControl     = controlObj [controlKey];
+
+  //--- Now set or clear the timer as appropriate.
+  if (btargetsFound  &&  bWaitOnce  &&  timeControl) {
+      //--- The only condition where we need to clear the timer.
+      clearInterval (timeControl);
+      delete controlObj [controlKey]
+  }
+  else {
+      //--- Set a timer, if needed.
+      if ( ! timeControl) {
+          timeControl = setInterval ( function () {
+                  waitForKeyElements (    selectorTxt,
+                                          actionFunction,
+                                          bWaitOnce,
+                                          iframeSelector
+                                      );
+              },
+              300
+          );
+          controlObj [controlKey] = timeControl;
+      }
+  }
+  waitForKeyElements.controlObj   = controlObj;
+}
 })();
